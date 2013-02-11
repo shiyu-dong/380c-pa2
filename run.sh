85cf3df8abb6e700a38d81756b833f371a1961e2
francis.zw@gmail.com <francis.zw@gmail.com@e0f387f0-838e-f11a-968b-3af9a0a737c9>
2013-02-11 06:16:00 +0000

    gcb working

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-# Script to run your translator.
+echo ../src/csc simple.c > simple.3addr
+../src/csc simple.c > simple.3addr
+echo python PA2.py simple.3addr > simple.3addr.c
+python PA2.py simple.3addr > simple.3addr.c
+echo gcc simple.3addr.c
+gcc simple.3addr.c
+echo ./a.out
+./a.out
